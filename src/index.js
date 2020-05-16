13e05c9806ec4a780d6dcdcd891d3a2a645d202f
William Stephan <william.stephan@icloud.com>
2020-05-15 21:56:48 -0500

    Adding back manifest

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+RReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
