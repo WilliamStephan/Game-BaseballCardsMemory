2ec2cd2bd1dd953fcbc448d1423e0ba7b39d55e5
William Stephan <william.stephan@icloud.com>
2020-05-15 20:19:47 -0500

    make it better

---
+++ build/precache-manifest.3da2209d5c00e0209b5c2258df4f2b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da24a5279787d73d4227b37648d18a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98db9c531ec48adb7d74",
+    "url": "/static/css/main.721d93d9.chunk.css"
+  },
+  {
+    "revision": "2074746341e203f7a9ab",
+    "url": "/static/js/2.6bcc4478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bcc4478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98db9c531ec48adb7d74",
+    "url": "/static/js/main.bb5baf58.chunk.js"
+  },
+  {
+    "revision": "bd6f02edca5afa4ef6e4",
+    "url": "/static/js/runtime-main.3a8769be.js"
+  }
+]);
